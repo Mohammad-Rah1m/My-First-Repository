8cd002dc36db4e7ee34890fbff028132db228154
Mohammad Rahim <rahimchangezi5@gmail.com>
2023-04-05 11:52:17 +0500

    changes

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
-console.log('hello world');
+var startTime, endTime;
+
+function start() {
+    startTime = new Date();
+  };
+
+  function end() {
+    endTime = new Date();
+    var timeDiff = endTime - startTime; //in ms
+    // strip the ms
+    timeDiff /= 1000;
+  
+    // get seconds 
+    var seconds = Math.round(timeDiff);
+    document.getElementById('time').innerHTML=seconds + " Seconds";
+  } 
